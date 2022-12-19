8558f0f66c38ca4b9fd5a908fa70b148d6bf3e9d
Brian Furaha <95090174+Brian-Furaha@users.noreply.github.com>
2022-12-19 21:27:48 +0300

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,3 @@
 <?php
 
-$conn = new mysqli('localhost', 'root', '', 'fb_db') or die("Could not connect to mysql" . mysqli_error($con));
+$conn = new mysqli('localhost', 'root', '', 'farmcust_db') or die("Could not connect to mysql" . mysqli_error($con));
